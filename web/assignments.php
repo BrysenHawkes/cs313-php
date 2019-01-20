688d8716d59cdf8cd5517bb211bd28c08176060f
Brysen Hawkes <bdhawkes@gmail.com>
2019-01-19 20:27:35 -0700

    added assignments and TTR photo

---
+++ web/assignments.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<meta charset = "utf-8" />
+    		<meta http-equiv = "X-UA-Compatible" content = "IE=edge" />
+    		<meta name = "viewport" content = "width=device-width, initial-scale=1"/>
+		<title>Homepage</title>
+		<link REL="StyleSheet" TYPE="text/css" HREF="style.css">
+		<script>
+		</script>
+	</head>
+	
+	<body>
+		<div class = "head";>
+			<h1>
+			  ASSIGNMENTS
+			</h1>
+		</div>
+		<div class = "topnav";>
+			<a href="homepage.php";>Home</a>
+			<a href="assignments.php";>Assignments</a>
+			<a href="https://www.igda.org/";>IGDA</a>
+		</div>
+		<div class = "sidebar";>
+			  <?php
+			  echo "Today is " . date("l") . "</br>";
+			  echo date("d/m/Y");
+			  ?>
+			</br>
+			<h3>Coming Soon</h3>
+			
+		</div>
+		<div class = "body";>
+		</div>
+		<div class = "foot";>
+		</div>
+	</body>
+</html>
--- web/homepage.php
+++ web/homepage.php
@@ -12,40 +12,40 @@
 	<body>
 		<div class = "head";>
 			<h1>
-			  HOME PAGE
+			  GAME DESIGN
 			</h1>
 		</div>
 		<div class = "topnav";>
-			<a href="#";>Link 1</a>
-			<a href="#";>Link 2</a>
-			<a href="#";>Link 3</a>
-			<a href="#";>Link 4</a>
-			<a href="#";>Link 5</a>
+			<a href="homepage.html";>Home</a>
+			<a href="assignments.php";>Assignments</a>
+			<a href="https://www.igda.org/";>IGDA</a>
 		</div>
 		<div class = "sidebar";>
 			  <?php
 			  echo "Today is " . date("l") . "</br>";
 			  echo date("d/m/Y");
 			  ?>
+			</br>
+			<h3>Game of the week</h3>
+			<img src="tickettoride.jpg" alt="Ticket to Ride, the board game" style="width:100%">
 		</div>
 		<div class = "body";>
+			<h3>Inroduction</h3>
 			<p>
-			  This is my webpage for my new web design class
+			  Game design is the art of applying design and aesthetics to create a game for entertainment or for educational, exercise, or experimental purposes. Increasingly, elements and principles of game design are also applied to other interactions, particularly virtual ones (see gamification).
 			</p>
 			<p>
-			  This is my webpage for my new web design class
+			  Game design creates goals, rules and challenges to define a board game, card game, dice game, casino game, role-playing game, sport, video game, war game or simulation that produces desirable interactions among its participants and, possibly, spectators.
 			</p>
 			<p>
-			  This is my webpage for my new web design class
+			  Academically, game design is part of game studies, while game theory studies strategic decision making (primarily in non-game situations). Games have historically inspired seminal research in the fields of probability, artificial intelligence, economics, and optimization theory. Applying game design to itself is a current research topic in metadesign.
 			</p>
+			<h3>Why I Like Game Design</h3>
 			<p>
-			  This is my webpage for my new web design class
+			I like to make things that other people enjoy. I enjoy art and have used this as a medium for expression before. However I find my skills limited within art. With Game Design I can make something that other people enjoy while also challenging myself mentally to make a simple game that is rich with strategy. It also allows me to tell a stroy without writing, which is a weakness of mine.
 			</p>
 		</div>
 		<div class = "foot";>
-			<p>
-			  Contact Us
-			</p>
 		</div>
 	</body>
 </html>
