7589ddbfc4dd09245187cf75a1ac57e0c633a315
Brysen Hawkes <bdhawkes@gmail.com>
2019-01-26 19:36:42 -0700

    fixing doorza.php

--- web/cart.php
+++ web/cart.php
@@ -25,32 +25,7 @@ echo ($_SESSION["qty"][3]);
 
 ?>
 <div class="product">
-	<?php
-	echo ("Cheese Pizza: ");
-	echo ($_SESSION["qty"][0]);
-	echo ("<a href="?delete=0">Add to Cart</a>");
-	?>
-</div>
-<div class="product">
-	<?php
-	echo ("Pepperoni Pizza: ");
-	echo ($_SESSION["qty"][1]);
-	echo ("<a href="?delete=1">Add to Cart</a>");
-	?>
-</div>
-<div class="product">
-	<?php
-	echo ("Fudge Brownie: ");
-	echo ($_SESSION["qty"][2]);
-	echo ("<a href="?delete=2">Add to Cart</a>");
-	?>
-</div>
-<div class="product">
-	<?php
-	echo ("Soda: ");
-	echo ($_SESSION["qty"][3]);
-	echo ("<a href="?delete=3">Add to Cart</a>");
-	?>
+	<h3>Hello</h3>
 </div>
 
 <div>
