28a0b39b8d5823ec31830e2a167c0fc628cb2f2c
Brysen Hawkes <bdhawkes@gmail.com>
2019-01-26 18:25:38 -0700

    fixing cart.php

--- web/cart.php
+++ web/cart.php
@@ -17,10 +17,10 @@ if ( isset($_GET["delete"]) )
 echo ("Cheese Pizza: ");
 echo ($_SESSION["qty"][0]);
 echo ("Pepperoni Pizza: ");
-echo ($_SESSION["qty"][0]);
+echo ($_SESSION["qty"][1]);
 echo ("Fudge Brownie: ");
-echo ($_SESSION["qty"][0]);
+echo ($_SESSION["qty"][2]);
 echo ("Soda: ");
-echo ($_SESSION["qty"][0]);
+echo ($_SESSION["qty"][3]);
 
 ?>
