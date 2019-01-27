71843aed12131fa6d27fa9008974dbc736fd2d68
Brysen Hawkes <bdhawkes@gmail.com>
2019-01-26 20:07:02 -0700

    fixing checkout.php

--- web/checkout.php
+++ web/checkout.php
@@ -1,28 +1,13 @@
-<html>
-	<head>
-		<meta charset = "utf-8" />
-    		<meta http-equiv = "X-UA-Compatible" content = "IE=edge" />
-    		<meta name = "viewport" content = "width=device-width, initial-scale=1"/>
-		<title>Checkout</title>
-		<link REL="StyleSheet" TYPE="text/css" HREF="style.css">
-		<script>
-		</script>
-	</head>
-	
-	<body>
-		<div class = "head";>
-			<h1>
-			  Checkout
-			</h1>
-		</div>
-		<div class = "topnav";>
-		</div>
-		<div class = "sidebar";>
-		</div>
-		<div class = "body";>
-			<h3>Enter Address</h3>
-		</div>
-		<div class = "foot";>
-		</div>
-	</body>
-</html>
+<?php
+
+session_start();
+
+?>
+
+<form id="adress" action="confirm.php" >
+	<input id="street" type="text"></br>
+	<input id="city" type="text"></br>
+	<input id="state" type="text"></br>
+	<input id="zip" type="text"></br>
+	<input id="submit" type="submit">
+</form>
