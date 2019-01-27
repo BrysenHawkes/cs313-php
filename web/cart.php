65963c1d3d8c4a6337642ff4205014a013d5f025
Brysen Hawkes <bdhawkes@gmail.com>
2019-01-26 19:57:38 -0700

    fixing doorza.php

--- web/cart.php
+++ web/cart.php
@@ -13,15 +13,6 @@ if ( isset($_GET["delete"]) )
    }
    $_SESSION["qty"][$i] = $qty;
   }
- 
-echo ("Cheese Pizza: ");
-echo ($_SESSION["qty"][0]);
-echo ("Pepperoni Pizza: ");
-echo ($_SESSION["qty"][1]);
-echo ("Fudge Brownie: ");
-echo ($_SESSION["qty"][2]);
-echo ("Soda: ");
-echo ($_SESSION["qty"][3]);
 
 ?>
 <div class="product">
