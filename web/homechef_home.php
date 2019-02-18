7fa924698e525ef818a2b204c36d2a55ea1d777a
Brysen Hawkes <bdhawkes@gmail.com>
2019-02-18 15:11:19 -0700

    adding homechef

---
+++ web/homechef_home.php
@@ -0,0 +1,51 @@
+<?php
+	try
+	{
+  		$dbUrl = getenv('DATABASE_URL');
+
+  		$dbOpts = parse_url($dbUrl);
+
+  		$dbHost = $dbOpts["host"];
+  		$dbPort = $dbOpts["port"];
+  		$dbUser = $dbOpts["user"];
+  		$dbPassword = $dbOpts["pass"];
+  		$dbName = ltrim($dbOpts["path"],'/');
+
+  		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+  		echo 'Error!: ' . $ex->getMessage();
+  		die();
+	}
+?>
+	<head>
+		<meta charset = "utf-8" />
+    		<meta http-equiv = "X-UA-Compatible" content = "IE=edge" />
+    		<meta name = "viewport" content = "width=device-width, initial-scale=1"/>
+		<title>Home Chef</title>
+		<link REL="StyleSheet" TYPE="text/css" HREF="style.css">
+		<script>
+		</script>
+	</head>
+	
+	<body>
+		<div class = "head";>
+			<h1>
+			  Home Chef
+			</h1>
+		</div>
+		<div class = "topnav";>
+			<a href="homechef_home.php";>Home</a>
+			<a href="homechef_home.php";>Meal Plans</a>
+			<a href="homechef_home.php";>Recipes</a>
+		</div>
+		<div class = "sidebar";>
+		</div>
+		<div class = "body";>
+		</div>
+		<div class = "foot";>
+		</div>
+	</body>
