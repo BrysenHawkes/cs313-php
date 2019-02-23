3057e94ce529153c07656f4dfae67a518b4564c1
Brysen Hawkes <bdhawkes@gmail.com>
2019-02-22 20:44:19 -0700

    added additional pages

--- web/homechef_recipes.php
+++ web/homechef_recipes.php
@@ -68,7 +68,7 @@
 						$stmt->bindValue(':id', $value, PDO::PARAM_INT);
 						$stmt->execute();
 						$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-						echo $rows;
+						echo $rows['name'];
   					}
 
   					echo '<br/><br/>';
