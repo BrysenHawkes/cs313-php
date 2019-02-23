645f123157c3808e5171bbc59fafcb5d76f12ac8
Brysen Hawkes <bdhawkes@gmail.com>
2019-02-22 19:41:46 -0700

    added additional pages

--- web/homechef_recipes.php
+++ web/homechef_recipes.php
@@ -46,9 +46,12 @@
 		</div>
 		<div class = "body";>
 			<?php
-				foreach ($db->query('SELECT ingredient_id FROM recipe') as $row)
+				foreach ($db->query('SELECT * FROM recipe') as $row)
 				{
+  					echo 'ingredient name: ' . $row['name'];
   					echo 'ingredient name: ' . $row['ingredient_id'];
+  					echo 'ingredient name: ' . $row['amount'];
+  					echo 'ingredient name: ' . $row['direction'];
   					echo '<br/>';
 				}
 			?>
