146ef991fd994404487bde6ee8e2d6d81ff16e42
Brysen Hawkes <bdhawkes@gmail.com>
2019-02-23 18:51:06 -0700

    add ingredient page

--- web/homechef_insertingredient.php
+++ web/homechef_insertingredient.php
@@ -21,8 +21,8 @@
   		die();
 	}
 
-	$ingredient_name = htmlspecialchars($_GET['name']);
-	$ingredient_price = htmlspecialchars($_GET['price']);
+	$ingredient_name = htmlspecialchars($_POST['name']);
+	$ingredient_price = htmlspecialchars($_POST['price']);
 
 	echo "$ingredient_name\n";
 	echo "$ingredient_price\n";
