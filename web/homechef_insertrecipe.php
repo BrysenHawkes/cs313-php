7322b3d276380857a6e1c4001e4ef7c067ce3daa
Brysen Hawkes <bdhawkes@gmail.com>
2019-02-23 20:52:44 -0700

    add ingredient page

--- web/homechef_insertrecipe.php
+++ web/homechef_insertrecipe.php
@@ -119,13 +119,13 @@ try
 	echo '<br/>' . $name;
 	echo '<br/>' . $directions;
 
-	$stmt = $db->prepare('INSERT INTO recipe(name,ingredient_id,amount,direction) VALUES (:recipe_name, :ingredient_id, :amount, :direction);');
-	$stmt->bindValue(':recipe_name', (string)$name, PDO::PARAM_STR);
-	$stmt->bindValue(':igredient_id', (string)$id_string, PDO::PARAM_STR);
-	$stmt->bindValue(':amount', (string)$amount_string, PDO::PARAM_STR);
-	$stmt->bindValue(':direction', (string)$directions, PDO::PARAM_STR);
+	//$stmt = $db->prepare('INSERT INTO recipe(name,ingredient_id,amount,direction) VALUES (:recipe_name, :ingredient_id, :amount, :direction);');
+	//$stmt->bindValue(':recipe_name', (string)$name, PDO::PARAM_STR);
+	//$stmt->bindValue(':igredient_id', (string)$id_string, PDO::PARAM_STR);
+	//$stmt->bindValue(':amount', (string)$amount_string, PDO::PARAM_STR);
+	//$stmt->bindValue(':direction', (string)$directions, PDO::PARAM_STR);
 	//$stmt->bindValue(':ingredient_price', $ingredient_price, PDO::PARAM_);
-	$stmt->execute();
+	//$stmt->execute();
 
 	echo '<br/>' . $id_string;
 	echo '<br/>' . $amount_string;
